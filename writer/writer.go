e1a5dcf69630227e8a5183c3ac8d1cf43625ea5f
MP Droog <mpdroog@icloud.com>
2016-05-26 17:35:21 +0200

    Feature. Output as JSON and msgpack based on HTTP accept or query ?accept

---
+++ writer/writer.go
@@ -0,0 +1,36 @@
+package writer
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+	"gopkg.in/vmihailenco/msgpack.v2"
+)
+
+func Encode(w http.ResponseWriter, r *http.Request, d interface{}) error {
+	accept := r.Header.Get("Accept")
+	if override := r.URL.Query().Get("accept"); override != "" {
+		// Browser override
+		accept = override
+	}
+
+	if accept == "application/json" {
+		str, e := json.Marshal(&d)
+		if e != nil {
+			log.Fatal(e)
+		}
+		w.Write([]byte(str))
+		return nil
+	}
+	if accept == "application/x-msgpack" {
+		b, e := msgpack.Marshal(&d)
+		if e != nil {
+			log.Fatal(e)
+		}
+		w.Write(b)
+		return nil
+	}
+
+	log.Fatal("Invalid accept=" + accept)
+	return nil
+}
