cd3c9382f70c9ae633193e5ec08ccc853a444917
MP Droog <mpdroog@icloud.com>
2016-05-26 17:34:58 +0200

    Feature. Run raw queries on DB and return results

---
+++ sql/model.go
@@ -0,0 +1,92 @@
+package sql
+
+import (
+	"database/sql"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"github.com/mpdroog/invoiced/writer"
+)
+
+var db *sql.DB
+
+func Init(d *sql.DB) error {
+	db = d
+	return nil
+}
+
+func get(sql string) ([]map[string]*string, error) {
+	var res []map[string]*string
+
+	rows, e := db.Query(sql)
+	if e != nil {
+		log.Fatal(e)
+	}
+	defer rows.Close()
+	cols, e := rows.Columns()
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	for rows.Next() {
+		ptr := make([]interface{}, len(cols))
+		ctx := make([]*string, len(cols))
+
+		for i := range ptr {
+			ptr[i] = &ctx[i]
+		}
+
+		if e := rows.Scan(ptr...); e != nil {
+			log.Fatal(e)
+		}
+
+		m := make(map[string]*string, len(cols))
+		for i := 0; i < len(cols); i++ {
+			m[cols[i]] = ctx[i]
+		}
+
+		res = append(res, m)
+	}
+	if e := rows.Err(); e != nil {
+		log.Fatal(e)
+	}
+
+	return res, e
+}
+
+func GetRow(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	sql := r.URL.Query().Get("sql")
+	if (sql == "") {
+		log.Fatal("Missing sql-arg")
+	}
+
+	res, e := get(sql)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	var rep map[string]*string
+	if len(res) > 0 {
+		rep = res[0]
+	}
+
+	if e := writer.Encode(w, r, rep); e != nil {
+		log.Fatal(e)
+	}
+}
+
+func GetAll(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	sql := r.URL.Query().Get("sql")
+	if (sql == "") {
+		log.Fatal("Missing sql-arg")
+	}
+
+	res, e := get(sql)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	if e := writer.Encode(w, r, res); e != nil {
+		log.Fatal(e)
+	}
+}
