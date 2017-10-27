fdd27006200ba14537cb5f0c4e69749a646238df
MP Droog <mpdroog@icloud.com>
2017-10-27 16:03:24 +0200

    Feature. (Unfinished) Timer

--- contrib/desktop/main.go
+++ contrib/desktop/main.go
@@ -7,16 +7,19 @@ import (
 	"github.com/getlantern/systray"
 	//"github.com/getlantern/systray/icon"
 	"github.com/skratchdot/open-golang/open"
-	"bufio"
 	"github.com/mitchellh/go-homedir"
-	//"github.com/lextoumbourou/idle"
-	"github.com/ctcpip/notifize"
-	"sync"
+	"github.com/lextoumbourou/idle"
+	//"github.com/ctcpip/notifize"
+	//"sync"
+	"os"
+	"time"
 )
 
 var (
 	verbose bool
 	dbPath string
+	timerStart *time.Time
+	minute time.Duration
 )
 
 func main() {
@@ -29,6 +32,11 @@ func main() {
 	flag.Parse()
 	if verbose {
 		fmt.Printf("Verbose-mode\n")
+	}
+
+	minute, e = time.ParseDuration("1m")
+	if e != nil {
+		panic(e)
 	}
 
 	systray.Run(onReady, nil)
@@ -47,65 +55,53 @@ func onReady() {
 		fmt.Printf("exec=%s %s\n", name, args)
 	}
 	cmd := exec.Command(name, args...)
+	cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
 
-	// stderr
-	stderr, e := cmd.StderrPipe()
-	if e != nil {
-		panic(e)
-	}
-	// stdout
-	stdout, e := cmd.StdoutPipe()
-	if e != nil {
-		panic(e)
-	}
+	stopChan := make(chan bool, 2)
 
-	stopChan := make(chan bool, 1)
-	wg := &sync.WaitGroup{}
-
-	wg.Add(1)
-	go func() {
-		defer stdout.Close()
-		defer wg.Done()
-		scanner := bufio.NewScanner(stdout)
-	    for scanner.Scan() {
-	        fmt.Printf("OUT>> %s\n", scanner.Text())
-	    }
-	    if e := scanner.Err(); e != nil {
-	    	fmt.Printf("scanner.Std: %s\n", e.Error())
-	    }
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer stderr.Close()
-		defer wg.Done()
-		scanner := bufio.NewScanner(stderr)
-	    for scanner.Scan() {
-	    	txt := scanner.Text()
-	        fmt.Printf("ERR>> %s\n", txt)
-	        notifize.Display("InvoiceD", txt, false, "")
-	    }
-	    if e := scanner.Err(); e != nil {
-	    	fmt.Printf("scanner.Err: %s\n", e.Error())
-	    }
-	}()
-
-	//systray.SetIcon(icon.Data)
 	systray.SetTitle("$$$")
 	systray.SetTooltip("InvoiceD")
 
+	mBrowser := systray.AddMenuItem("Open", "Open browser")
+	mStart := systray.AddMenuItem("Start", "Start timer")
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
-	mBrowser := systray.AddMenuItem("Open", "")
 
 	go func() {
 		if e := cmd.Start(); e != nil {
 			panic(e)
 		}
-		wg.Wait()
 		if e := cmd.Wait(); e != nil {
 			fmt.Printf("cmd.Wait: %s\n", e.Error())
 		}
 		stopChan <- true
+	}()
+
+	// User timer
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				if verbose {
+					fmt.Println("interval.sec")
+				}
+				if timerStart != nil {
+					n := time.Now().Sub(*timerStart)
+					systray.SetTitle(fmt.Sprintf("$%02d:%02d:%02d", int(n.Hours())%60, int(n.Minutes())%60, int(n.Seconds())%60))
+				}
+				d, e := idle.Get()
+				if e != nil {
+					panic(e)
+				}
+				if d > minute { //time.Duration("5m") {
+					fmt.Printf("1min idle\n")
+				}
+			case <-stopChan:
+				ticker.Stop()
+				return
+			}
+		}
 	}()
 
 	for {
@@ -130,22 +126,13 @@ func onReady() {
 			if e := open.Run("http://localhost:9999/static/"); e != nil {
 				panic(e)
 			}
+
+		case <-mStart.ClickedCh:
+			if verbose{
+				fmt.Println("cmd=start")
+			}
+			n := time.Now()
+			timerStart = &n
 		}
 	}
-
-	// User timer
-	/*go func() {
-		for {
-			select {
-			case <-timer:
-				if verbose {
-					fmt.Println("user-timer")
-				}
-				d, e := idle.Get()
-				if d > time.Duration("5m") {
-					// TODO
-				}
-			}
-		}
-	}()*/
 }
