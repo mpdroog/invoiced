8257e08c1cfe196d3b5dc5d5a8436817f4b9578d
mark <mpdroog@rootdev.nl>
2026-01-11 17:19:40 +0100

    Bugfix. React crashing on utility funcs

--- static/assets/prototype.js
+++ static/assets/prototype.js
@@ -1,44 +1,57 @@
 // Small bunch of prototype functions to rapidly
 // play with data in memory
 //
+// Using defineProperty to make methods non-enumerable (React compatible)
 
-// Convert object to array and return subitems as one big array
-Object.prototype.concatArray = function() {
-	var n = Object.values(this);
-	var out = [];
-	for (i = 0; i < n.length; i++) {
-		out = out.concat(n[i]);
-	}
-	return out;
-}
-
-Array.prototype.filterField = function(fieldname) {
-	var out = [];
-	var keys = fieldname.split(".");
-	for (var i = 0; i < this.length; i++) {
-		var base = this[i];
-		for (var n = 0; n < keys.length; n++) {
-			base = base[ keys[n] ];
+Object.defineProperty(Object.prototype, 'concatArray', {
+	enumerable: false,
+	value: function() {
+		var n = Object.values(this);
+		var out = [];
+		for (var i = 0; i < n.length; i++) {
+			out = out.concat(n[i]);
 		}
-		out.push(base);
+		return out;
 	}
-	return out;
-}
-Array.prototype.remove = function(id) {
-	// ignore res
-	this.splice(id, 1);
-	return this;
-}
+});
 
-Array.prototype.sum = function(round) {
-	var out = new Big("0.00");
-
-	for (var i = 0; i < this.length; i++) {
-		out = out.plus(new Big(this[i]));
+Object.defineProperty(Array.prototype, 'filterField', {
+	enumerable: false,
+	value: function(fieldname) {
+		var out = [];
+		var keys = fieldname.split(".");
+		for (var i = 0; i < this.length; i++) {
+			var base = this[i];
+			for (var n = 0; n < keys.length; n++) {
+				base = base[ keys[n] ];
+			}
+			out.push(base);
+		}
+		return out;
 	}
+});
 
-	return out.round(round).toFixed(round).toString();
-}
+Object.defineProperty(Array.prototype, 'remove', {
+	enumerable: false,
+	value: function(id) {
+		// ignore res
+		this.splice(id, 1);
+		return this;
+	}
+});
+
+Object.defineProperty(Array.prototype, 'sum', {
+	enumerable: false,
+	value: function(round) {
+		var out = new Big("0.00");
+
+		for (var i = 0; i < this.length; i++) {
+			out = out.plus(new Big(this[i]));
+		}
+
+		return out.round(round).toFixed(round).toString();
+	}
+});
 
 console.log(`
 Total ex VAT this year:
